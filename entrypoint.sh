64547b39bb6036c90adfbc239da1673a68762a8f
Shane Parker <shparker1977@users.noreply.github.com>
2018-12-11 11:46:10 -0600

    Commented out sed which causes error in k8s

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-if [[ -z ${FLUENT_ELASTICSEARCH_USER} ]] ; then
-   sed -i  '/FLUENT_ELASTICSEARCH_USER/d' /fluentd/etc/${FLUENTD_CONF}
-fi
+## Commenting out sed on RO filesystem.
+#if [[ -z ${FLUENT_ELASTICSEARCH_USER} ]] ; then
+#   sed -i  '/FLUENT_ELASTICSEARCH_USER/d' /fluentd/etc/${FLUENTD_CONF}
+#fi
 
-if [[ -z ${FLUENT_ELASTICSEARCH_PASSWORD} ]] ; then
-   sed -i  '/FLUENT_ELASTICSEARCH_PASSWORD/d' /fluentd/etc/${FLUENTD_CONF}
-fi
+#if [[ -z ${FLUENT_ELASTICSEARCH_PASSWORD} ]] ; then
+#   sed -i  '/FLUENT_ELASTICSEARCH_PASSWORD/d' /fluentd/etc/${FLUENTD_CONF}
+#fi
 
 exec fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins ${FLUENTD_OPT}
